cfb20457c0df7d09147185023fc6dd17abe01d12
Rafael Perrella <rafael.clp@hotmail.com>
2013-11-11 23:39:24 -0300

    Mensagem ao digitar string
    Se digitar string em vez de numero, recebe uma mensagem pedindo para
    digitar novamente.

--- lab1/NumeroPorExtenso/src/NumeroPorExtenso/InterfaceDeUsuario.java
+++ lab1/NumeroPorExtenso/src/NumeroPorExtenso/InterfaceDeUsuario.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class InterfaceDeUsuario {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		System.out.println("Digite um número de 0 a 1 bilhão:");
 		do {
-			System.out.println("Digite um número de 0 a 1 bilhão:");
 			try {
 				int numero = Integer.parseInt(in.next());
 				System.out.println(NumeroPorExtenso.converter(numero));
 				break;
 			} catch (Exception e) {
-				
+				System.out.println("Voce deve digitar um numero (de 0 a 1 bilhao). Digite novamente.");
 			}
 		} while (true);
 	}
