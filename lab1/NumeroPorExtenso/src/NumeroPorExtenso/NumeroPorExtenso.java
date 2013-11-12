cd9803c366baa673a0fc8bc2f50e2736bafa2e89
Rafael Perrella <rafael.clp@hotmail.com>
2013-11-11 23:31:57 -0300

    Numeros de 3 algarismos

--- lab1/NumeroPorExtenso/src/NumeroPorExtenso/NumeroPorExtenso.java
+++ lab1/NumeroPorExtenso/src/NumeroPorExtenso/NumeroPorExtenso.java
@@ -8,7 +8,7 @@ public class NumeroPorExtenso {
 		}
 		return algarismos[algarismo];
 	}
-	private static String converterNumeroDe2Digitos(int numero) throws Exception {
+	private static String converterNumeroDeAte2Digitos(int numero) throws Exception {
 		String[] numeros10A19 = {"dez", "onze", "doze", "treze", "catorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"};
 		String[] dezenas = {"vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"};
 
@@ -32,12 +32,36 @@ public class NumeroPorExtenso {
 		return resultado;
 	}
 
+	private static boolean jaContemE(String numeroParcial) throws Exception {
+		return numeroParcial.contains(" e ");
+	}
+
+	private static String converterNumeroDeAte3Digitos(int numero) throws Exception {
+		String[] centenas = {"cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"};
+
+		String resultado = "";
+
+		if (numero <= 99) {
+			resultado = converterNumeroDeAte2Digitos(numero);
+		} else if (numero == 100) {
+			resultado = "cem";
+		} else if (numero <= 999) {
+			resultado = centenas[numero/100-1];
+			numero %= 100;
+			if (numero != 0) {
+				String ultimos2digitos = converterNumeroDeAte2Digitos(numero);
+				if (!jaContemE(ultimos2digitos)) {
+					resultado += " e ";
+				}
+				resultado += ultimos2digitos;
+			}
+		}
+		return resultado;
+	}
+
 	public static String converter(int numero) {
 		try {
-			if (numero < 10)
-				return converterDigito(numero);
-			else
-				return converterNumeroDe2Digitos(numero);
+			return converterNumeroDeAte3Digitos(numero);
 		} catch (Exception e) {
 			System.err.println("Não implementado ainda!");
 		}
--- lab1/NumeroPorExtenso/src/NumeroPorExtenso/NumeroPorExtensoTest.java
+++ lab1/NumeroPorExtenso/src/NumeroPorExtenso/NumeroPorExtensoTest.java
@@ -57,4 +57,45 @@ public class NumeroPorExtensoTest {
 		assertEquals(NumeroPorExtenso.converter(98), "noventa e oito");
 		assertEquals(NumeroPorExtenso.converter(99), "noventa e nove");
 	}
+
+	@Test
+	public void numerosDeTresAlgarismos() {
+		assertEquals(NumeroPorExtenso.converter(100), "cem");
+		assertEquals(NumeroPorExtenso.converter(101), "cento e um");
+		assertEquals(NumeroPorExtenso.converter(102), "cento e dois");
+		assertEquals(NumeroPorExtenso.converter(103), "cento e três");
+		assertEquals(NumeroPorExtenso.converter(104), "cento e quatro");
+		assertEquals(NumeroPorExtenso.converter(105), "cento e cinco");
+		assertEquals(NumeroPorExtenso.converter(106), "cento e seis");
+		assertEquals(NumeroPorExtenso.converter(107), "cento e sete");
+		assertEquals(NumeroPorExtenso.converter(108), "cento e oito");
+		assertEquals(NumeroPorExtenso.converter(109), "cento e nove");
+		assertEquals(NumeroPorExtenso.converter(110), "cento e dez");
+		assertEquals(NumeroPorExtenso.converter(111), "cento e onze");
+		assertEquals(NumeroPorExtenso.converter(112), "cento e doze");
+		assertEquals(NumeroPorExtenso.converter(113), "cento e treze");
+		assertEquals(NumeroPorExtenso.converter(114), "cento e catorze");
+		assertEquals(NumeroPorExtenso.converter(115), "cento e quinze");
+		assertEquals(NumeroPorExtenso.converter(116), "cento e dezesseis");
+		assertEquals(NumeroPorExtenso.converter(117), "cento e dezessete");
+		assertEquals(NumeroPorExtenso.converter(118), "cento e dezoito");
+		assertEquals(NumeroPorExtenso.converter(119), "cento e dezenove");
+		assertEquals(NumeroPorExtenso.converter(120), "cento e vinte");
+		assertEquals(NumeroPorExtenso.converter(200), "duzentos");
+		assertEquals(NumeroPorExtenso.converter(201), "duzentos e um");
+		assertEquals(NumeroPorExtenso.converter(300), "trezentos");
+		assertEquals(NumeroPorExtenso.converter(301), "trezentos e um");
+		assertEquals(NumeroPorExtenso.converter(400), "quatrocentos");
+		assertEquals(NumeroPorExtenso.converter(401), "quatrocentos e um");
+		assertEquals(NumeroPorExtenso.converter(500), "quinhentos");
+		assertEquals(NumeroPorExtenso.converter(501), "quinhentos e um");
+		assertEquals(NumeroPorExtenso.converter(600), "seiscentos");
+		assertEquals(NumeroPorExtenso.converter(601), "seiscentos e um");
+		assertEquals(NumeroPorExtenso.converter(700), "setecentos");
+		assertEquals(NumeroPorExtenso.converter(701), "setecentos e um");
+		assertEquals(NumeroPorExtenso.converter(800), "oitocentos");
+		assertEquals(NumeroPorExtenso.converter(801), "oitocentos e um");
+		assertEquals(NumeroPorExtenso.converter(900), "novecentos");
+		assertEquals(NumeroPorExtenso.converter(901), "novecentos e um");
+	}
 }
