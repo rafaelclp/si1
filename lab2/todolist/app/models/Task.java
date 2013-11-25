373b0e69ab40434ff5a072b65f6bcd64093149be
Rafael Perrella <rafael.clp@hotmail.com>
2013-11-25 13:08:40 -0300

    Substituidos espaços por tabs e linhas grandes quebradas

--- lab2/todolist/app/models/Task.java
+++ lab2/todolist/app/models/Task.java
@@ -10,27 +10,24 @@ import javax.persistence.*;
 
 @Entity
 public class Task extends Model {
+	@Id
+	public Long id;
 
-  @Id
-  public Long id;
+	@Required
+	public String label;
 
-  @Required
-  public String label;
-  
-  public static Finder<Long,Task> find = new Finder<Long,Task>(
-		  Long.class, Task.class
-  );
+	public static Finder<Long, Task> find = new Finder<Long, Task>(Long.class,
+			Task.class);
 
-  public static List<Task> all() {
-	  return find.all();
-  }
-  
-  public static void create(Task task) {
-	  task.save();
-  }
-  
-  public static void delete(Long id) {
-	  find.ref(id).delete();
-  }
- 
+	public static List<Task> all() {
+		return find.all();
+	}
+
+	public static void create(Task task) {
+		task.save();
+	}
+
+	public static void delete(Long id) {
+		find.ref(id).delete();
+	}
 }
