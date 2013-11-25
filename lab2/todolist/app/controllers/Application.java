cb1759ab8f515dae8ada376070bcd9dff09a005b
Rafael Perrella <rafael.clp@hotmail.com>
2013-11-25 03:12:49 -0300

    Formulário de tarefas adicionado

--- lab2/todolist/app/controllers/Application.java
+++ lab2/todolist/app/controllers/Application.java
@@ -1,14 +1,30 @@
 package controllers;
 
+import models.*;
 import play.*;
+import play.data.*;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
 
+	static Form<Task> taskForm = Form.form(Task.class);
+
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+    	return redirect(routes.Application.tasks());
+        //return ok(index.render("Your new application is ready."));
     }
 
+    public static Result tasks() {
+    	return ok(views.html.index.render(Task.all(), taskForm));
+    }
+      
+    public static Result newTask() {
+        return TODO;
+    }
+      
+    public static Result deleteTask(Long id) {
+        return TODO;
+    }
 }
